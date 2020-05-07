4ed66df0994c118402690e1c2958d96536daef5d
vincent <vincent@vincentdeMacBook-Pro.local>
2020-05-07 22:53:08 +0800

    增加then方法,增加两个内部数组以存放成功与失败callback

--- promise.js
+++ promise.js
@@ -9,6 +9,11 @@ class MyPromise {
       this._status = 'PENDING'
       // 添加状态
       this._value = undefined
+
+      // 添加成功回调函数队列
+      this._fulfilledQueues = []
+      // 添加失败回调函数队列
+      this._rejectedQueues = []
   
       // 执行handle
       try {
@@ -31,4 +36,25 @@ class MyPromise {
       this._status = 'REJECTED'
       this._value = err
     }
+
+    then (onFulfilled, onRejected) {
+        const { _value, _status } = this
+        switch (_status) {
+          // 当状态为pending时，将then方法回调函数加入执行队列等待执行
+          case 'PENDING':
+            this._fulfilledQueues.push(onFulfilled)
+            this._rejectedQueues.push(onRejected)
+            break
+          // 当状态已经改变时，立即执行对应的回调函数
+          case 'FULFILLED':
+            onFulfilled(_value)
+            break
+          case 'REJECTED':
+            onRejected(_value)
+            break
+        }
+        // 返回一个新的Promise对象
+        return new MyPromise((onFulfilledNext, onRejectedNext) => {})
+    }
 }
+  
