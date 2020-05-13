993914d59e188f85a8881bd10d005e01cee78d7e
vincent <vincent@vincentdeMacBook-Pro.local>
2020-05-13 16:21:04 +0800

    update

--- generator.js
+++ generator.js
@@ -1,3 +1,19 @@
+function createIterator(items) {
+  var i = 0
+  return {
+    next: function() {
+      var done = (i >= items.length)
+      var value = !done ? items[i++] : undefined
+      return {
+        done: done,
+        value: value
+      }
+    }
+  }
+}
+
+
+
 // Object.prototype[Symbol.iterator] = function () {
 //   var i = 0
 //   var items = Object.entries(this)
