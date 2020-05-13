17eaf687536ee51882b747a0e1d24ffa0f284101
vincent <vincent@vincentdeMacBook-Pro.local>
2020-05-13 16:15:35 +0800

    udpate

--- generator.js
+++ generator.js
@@ -1,3 +1,18 @@
+// Object.prototype[Symbol.iterator] = function () {
+//   var i = 0
+//   var items = Object.entries(this)
+//   return {
+//     next: function () {
+//       var done = (i >= items.length)
+//       var value = !done ? items[i++] : undefined
+//       return {
+//         done: done,
+//         value: value
+//       }
+//     }
+//   }
+// }
+
 Object.prototype[Symbol.iterator] = function* () {
   for (const key in this) {
     if (this.hasOwnProperty(key)) {
